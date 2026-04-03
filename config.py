f692b20cd274fb17b80e1a197ec97d6dfaeaf108
abutalhamollaher33 <abutalhamollah789talha@gmail.com>
2026-04-03 10:41:40 +0000

    Add bot configuration file

---
+++ config.py
@@ -0,0 +1,16 @@
+# Bot Configuration
+
+# Telegram Bot Token
+TOKEN = 'YOUR_TOKEN_HERE'
+
+# Admin ID
+ADMIN_ID = YOUR_ADMIN_ID_HERE
+
+# Group IDs
+GROUP_IDS = [GROUP_ID_1, GROUP_ID_2]
+
+# MongoDB Connection String
+MONGODB_URI = 'YOUR_MONGODB_CONNECTION_STRING_HERE'
+
+# Other Settings
+# Any additional settings can be placed here
